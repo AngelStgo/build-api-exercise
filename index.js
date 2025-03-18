2bc864000a13e20dbe5a41d0649c0d1968a0070d
AngelStgo <euangel.stgo@gmail.com>
2025-03-18 09:48:44 -0400

    added student's grades data array

---
+++ index.js
@@ -0,0 +1,28 @@
+import express, { Router } from "express";
+import dotenv from "dotenv";
+import router from "./routes/grades.js";
+dotenv.config();
+
+// console.log(process.env.ATLAS_URI)
+
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API.");
+});
+
+app.use("/grades", router ); 
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("Seems like we messed up somewhere...");
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
---
+++ routes/grades.js
@@ -0,0 +1,30 @@
+import express from "express";
+import db from "../db/conn.js";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+// GET /grades/:id
+// Get a single grade entry
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+
+// Get a student's grade data
+router.get("/student/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { student_id: Number(req.params.id) };
+  let result = await collection.find(query).toArray();
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+
+export default router;
