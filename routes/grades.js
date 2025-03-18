77c492856dbbb4f6e3c3ba21a29efc09be65ea98
AngelStgo <euangel.stgo@gmail.com>
2025-03-18 11:14:42 -0400

    new patch for add and remove score and grades

--- routes/grades.js
+++ routes/grades.js
@@ -15,6 +15,12 @@ router.get("/:id", async (req, res) => {
   else res.send(result).status(200);
 });
 
+// Student route for backwards compatibility
+// ".redirect" Since we changed the name from student to learner
+router.get("/student/:id", async (req, res) => {
+  res.redirect(`/grades/learner/${req.params.id}`); // need a prefix
+});
+
 // GET /grades/learner/:id
 // Get a single student's grade data
 router.get("/learner/:id", async (req, res) => {
@@ -26,12 +32,67 @@ router.get("/learner/:id", async (req, res) => {
   else res.send(result).status(200);
 });
 
+
+
 // GET /grades/class/:id
 // Get a class's grade data
 router.get("/class/:id", async (req, res) => {
   let collection = await db.collection("grades");
   let query = { class_id: Number(req.params.id) };
   let result = await collection.find(query).toArray();
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+
+// Create a single grade entry
+router.post("/", async (req, res) => {
+  let collection = await db.collection("grades");
+  let newDocument = req.body;
+
+  // rename fields for backwards compatibility
+  if (newDocument.student_id) {
+    newDocument.learner_id = newDocument.student_id;
+    delete newDocument.student_id;
+  }
+
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+});
+
+
+// Add a score to a grade entry
+router.patch("/:id/add", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { _id: new ObjectId(req.params.id) };
+
+  let result = await collection.updateOne(query, {
+    $push: { scores: req.body },
+  });
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// Remove a score from a grade entry
+router.patch("/:id/remove", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { _id: new ObjectId(req.params.id) };
+
+  let result = await collection.updateOne(query, {
+    $pull: { scores: req.body },
+  });
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// Delete a single grade entry
+router.delete("/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.deleteOne(query);
 
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
